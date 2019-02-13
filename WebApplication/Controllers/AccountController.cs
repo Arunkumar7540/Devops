3a4e76023010e2a27318082543233fa8cc0f8b1c
getratheeshar@gmail.com <getratheeshar@gmail.com>
2019-02-13 07:12:03 +0000

    Updated AccountController.cs

--- WebApplication/Controllers/AccountController.cs
+++ WebApplication/Controllers/AccountController.cs
@@ -22,7 +22,7 @@ namespace WebApplication.Controllers
         {
         }
 
-        //Account Controller
+        //Account Controller1
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
         {
             UserManager = userManager;
